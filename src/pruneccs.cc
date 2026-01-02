c0edba49c35baa95e8eb6192204cd4c1e7105061
Akash Levy <akash@silimate.com>
2026-01-02 03:57:24 -0500

    Clean up main

--- src/pruneccs.cc
+++ src/pruneccs.cc
@@ -5,26 +5,28 @@
 using namespace sta;
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Usage: prunecss <file.lib>\n";
-        return 1;
-    }
+  // Get arguments
+  if (argc != 2) {
+    std::cout << "Usage: pruneccs <file.lib>\n";
+    return 1;
+  }
+  const char *filename = argv[1];
 
-    const char *filename = argv[1];
+  // Initialize OpenSTA environment
+  sta::initSta();
+  Sta *sta = new Sta();
+  Sta::setSta(sta);
+  sta->makeComponents();
 
-    // Initialize OpenSTA environment
-    sta::initSta();
-    Sta *sta = new Sta();
-    Sta::setSta(sta);
-    sta->makeComponents();
+  // Run the parser
+  STALibertyParser parser(filename);
 
-    // Run the parser
-    STALibertyParser parser(filename);
+  // Check if there was an error parsing the file
+  if (parser.check()) {
+    std::cerr << "ERROR: " << parser.get_error_text() << std::endl;
+    return 1;
+  }
 
-    if (parser.check()) {
-        std::cerr << "ERROR: " << parser.get_error_text() << std::endl;
-        return 1;
-    }
-
-    return 0;
+  // Success
+  return 0;
 }
